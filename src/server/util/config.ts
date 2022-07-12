fba6e2b120f5ffc3e93ebe0245c5f980e9db52b6
FlyingCat-X <flyingcatdm@gmail.com>
2022-07-11 23:04:10 -0500

    Add properties for discord event handlers

--- src/server/util/config.ts
+++ src/server/util/config.ts
@@ -19,41 +19,46 @@ export class Config {
 		oauth: {
 			clientID: "" as string,
 			clientSecret: "" as string,
-		}
+		},
 	};
 
 	public discord = {
 		oauth: {
 			clientID: "" as string,
-			clientSecret: "" as string
+			clientSecret: "" as string,
 		},
 		token: "" as string,
 		guildID: "" as string,
 		logChannel: "" as string,
 		roles: {
 			verifiedRole: "" as string,
-            groupRoles: {
-                gmt: "" as string,
-                bng: "" as string,
-                nat: "" as string,
-                alumni: "" as string,
-            },
-            playModeRoles: {
-                osu: "" as string,
-                mania: "" as string,
-                taiko: "" as string,
-                fruits: "" as string,
-            },
-            rankedMapper: "" as string,
-            isRestricted: "" as string,
-            foreigner: "" as string,
-		}
-	}
+			groupRoles: {
+				gmt: "" as string,
+				bng: "" as string,
+				nat: "" as string,
+				alumni: "" as string,
+			},
+			playModeRoles: {
+				osu: "" as string,
+				mania: "" as string,
+				taiko: "" as string,
+				fruits: "" as string,
+			},
+			rankedMapper: "" as string,
+			isRestricted: "" as string,
+			foreigner: "" as string,
+		},
+		eventHandlers: {
+			channels: {
+				mappingTimestamp: [""] as string[],
+			},
+		},
+	};
 
 	public mongo = {
 		uri: "" as string,
 		backupDir: "./data/backups" as string,
-	}
+	};
 
 	public misc = {
 		timezone: "America/Lima" as string,
@@ -66,31 +71,34 @@ export class Config {
 			info: "17A2B8" as string,
 			success: "28A745" as string,
 			warn: "FFC107" as string,
-			error: "DC3545" as string
-		}
-	}
+			error: "DC3545" as string,
+		},
+	};
 
 	public session = {
 		secret: "" as string,
-	}
+	};
 
 	constructor() {
 		this.load();
 	}
 
 	public load(configPath = "config.json"): void {
-		this.parse(fs.readFileSync(configPath).toString(), fs.readFileSync("config.default.json").toString());
+		this.parse(
+			fs.readFileSync(configPath).toString(),
+			fs.readFileSync("config.default.json").toString()
+		);
 	}
 
 	public parse(content: string, defaultContent: string): void {
-        const contentJSON = JSON.parse(content);
-        const defaultContentJSON = JSON.parse(defaultContent);
-        const configObj = _.defaultsDeep(contentJSON, defaultContentJSON);
+		const contentJSON = JSON.parse(content);
+		const defaultContentJSON = JSON.parse(defaultContent);
+		const configObj = _.defaultsDeep(contentJSON, defaultContentJSON);
 
-        Object.entries(configObj).forEach(([key, value]) => {
-            this[key] = value;
-        });
+		Object.entries(configObj).forEach(([key, value]) => {
+			this[key] = value;
+		});
 
-        Settings.defaultZone = this.misc.timezone;
-    }
+		Settings.defaultZone = this.misc.timezone;
+	}
 }
