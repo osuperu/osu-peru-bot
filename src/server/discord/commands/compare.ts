cc69e6f6f17ae2ff77af83e22b04ca421150c5fe
FlyingCat-X <flyingcatdm@gmail.com>
2022-07-14 22:22:15 -0500

    Add compare slash command

---
+++ src/server/discord/commands/compare.ts
@@ -0,0 +1,140 @@
+import { SlashCommand, SlashCommandReturn } from "../models/slash-command";
+import { User } from "../../models/user";
+import { OGamemodeName } from "../../models/osu-api/gamemode";
+import { OUser } from "../../models/osu-api/user";
+import { OScores } from "../../models/osu-api/score";
+import { OBeatmap } from "../../models/osu-api/beatmap";
+import { OsuApi } from "../../util/api/osu-api";
+import { CompareResponse } from "../responses/compare";
+
+export default <SlashCommand>{
+	commandEnum: "COMPARE",
+	name: "compare",
+	description: "Compara tu puntuacion con la ultima enviada en el canal.",
+	options: [
+		{
+			name: "user",
+			description:
+				"Nombre de usuario o ID del jugador cuya jugada reciente quieres ver",
+			type: "STRING",
+			required: false,
+		},
+		{
+			name: "discord",
+			description:
+				"Especifica un usuario de discord verificado en el servidor",
+			type: "USER",
+			required: false,
+		},
+		{
+			name: "gamemode",
+			description:
+				"Modo de juego de juego de la jugada a comparar. Por defecto se buscan jugadas de osu!standard",
+			type: "STRING",
+			choices: [
+				{
+					name: "osu!standard",
+					value: "osu",
+				},
+				{
+					name: "osu!taiko",
+					value: "taiko",
+				},
+				{
+					name: "osu!catch",
+					value: "fruits",
+				},
+				{
+					name: "osu!mania",
+					value: "mania",
+				},
+			],
+			required: false,
+		},
+	],
+	async call({ interaction }): Promise<SlashCommandReturn> {
+		try {
+			const guildMember =
+				interaction.options.getUser("discord", false) ||
+				interaction.member.user;
+			const userDb = await User.findOne({
+				"discord.userId": guildMember.id,
+			});
+
+			if (!userDb) {
+				return {
+					message: {
+						content:
+							"El usuario no tiene ninguna cuenta de osu! vinculada.",
+					},
+				};
+			}
+
+			const user =
+				interaction.options.getString("user", false) ||
+				(userDb ? userDb.osu.userID.toString() : null);
+			const gamemode = (interaction.options.getString(
+				"gamemode",
+				false
+			) || "osu") as OGamemodeName;
+
+			const messages = await interaction.channel.messages
+				.fetch({ limit: 100 })
+				.then((messageMappings) => {
+					return Array.from(messageMappings.values());
+				});
+
+			const regexp = /\w+\.ppy\.sh\/(?:b\/|beatmapsets\/\d+#\w+\/)(\d+)/g;
+			let beatmapID: number;
+			for (const message of messages) {
+				if (message.embeds.length > 0) {
+					for (const embed of message.embeds) {
+						const match = regexp.exec(embed.url);
+
+						if (match !== null) {
+							beatmapID = parseInt(match[1]);
+							break;
+						}
+					}
+				}
+
+				const match = regexp.exec(message.content);
+
+				if (match !== null) {
+					beatmapID = parseInt(match[1]);
+					break;
+				}
+			}
+
+			const osuUser = (await OsuApi.fetchUserPublic(
+				user,
+				gamemode
+			)) as OUser;
+
+			const beatmap = (await OsuApi.fetchBeatmap(
+				beatmapID
+			)) as OBeatmap;
+
+			const ret = (await OsuApi.fetchUserBeatmapScores(
+				beatmapID,
+				osuUser.id,
+				gamemode
+			)) as OScores;
+
+			return {
+				message: await new CompareResponse().getMessage(
+					osuUser,
+					beatmap,
+					ret,
+					gamemode
+				),
+			};
+		} catch (e) {
+			return {
+				message: {
+					content: "No se ha encontrado ningun beatmap a comparar.",
+				},
+			};
+		}
+	},
+};
---
+++ src/server/discord/responses/compare.ts
@@ -0,0 +1,58 @@
+import { MessageOptions } from "discord.js";
+import { CommandResponse } from "../../models/command-response";
+import { OUser } from "../../models/osu-api/user";
+import { OBeatmap } from "../../models/osu-api/beatmap";
+import { OScores } from "../../models/osu-api/score";
+import { OGamemodeName } from "../../models/osu-api/gamemode";
+import { Misc } from "../../util/misc";
+
+export class CompareResponse implements CommandResponse {
+	async getMessage(
+		user: OUser,
+		beatmap: OBeatmap,
+		scores: OScores,
+		gamemode: OGamemodeName
+	): Promise<MessageOptions> {
+		let description = "";
+		let count = 1;
+
+		if (scores.scores.length === 0) {
+			return {
+				content: `${user.username} no tiene puntuciones en el mapa.`,
+			};
+		}
+		for (const score of scores.scores) {
+			const mods = score.mods.length > 0 ? ` ${score.mods.join("")}` : "";
+
+			description +=
+				`**${count}.** ${mods}` +
+				"\n" +
+				`${score.rank} • ${
+					score.pp ? score.pp.toFixed(0) : undefined
+				}pp › ${(score.accuracy * 100).toFixed(2)}% › <${
+					score.max_combo
+				}x/${beatmap.max_combo}x> › \`[${Misc.getHitsFor(
+					gamemode,
+					score
+				)}]\`` +
+				"\n" +
+				`Hace ${Misc.timeFromNow(score.created_at)}` +
+				"\n";
+
+			count++;
+		}
+
+		return {
+			embeds: [
+				{
+					author: {
+						icon_url: user.avatar_url,
+						name: `Top de jugadas de ${user.username} en ${beatmap.beatmapset.artist} - ${beatmap.beatmapset.title} [${beatmap.version}]| osu!${gamemode}`,
+					},
+					url: `https://osu.ppy.sh/b/${beatmap.id}`,
+					description: description,
+				},
+			],
+		};
+	}
+}
