95a871c1ffe14baa775480f7c99d67979c2231b4
FlyingCat-X <flyingcatdm@gmail.com>
2022-07-20 17:33:49 -0500

    Add methods related to emojis and dates

--- src/server/util/misc.ts
+++ src/server/util/misc.ts
@@ -12,6 +12,10 @@ export class Misc {
 		return val != null && val !== "" && !isNaN(Number(val.toString()));
 	}
 
+	static toNumberWithCommas(_number: number): string {
+		return _number.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+	}
+
 	static convertDaysToYearDays(days: number): string {
 		const years = Math.floor(days / 365);
 		const remainingDays = Math.round(days % 365);
@@ -27,6 +31,10 @@ export class Misc {
 
 	static timeFromNow(date: Timestamp): string {
 		return moment(date).fromNow(); // Por mejorar para que la fecha de manera mas exacta
+	}
+
+	static convertToUnixTimestamp(date: Timestamp): number {
+		return moment(date).unix();
 	}
 
 	static getHitsFor(gamemode: OGamemodeName, score: OScore): string {
@@ -56,5 +64,18 @@ export class Misc {
 				return `${score.statistics.count_300}/${score.statistics.count_geki}/${score.statistics.count_100}/${score.statistics.count_katu}/${score.statistics.count_50}/${score.statistics.count_miss}`;
 			}
 		}
+	}
+
+	static getEmojiID(name: string): string {
+		const emojis: unknown = {
+			osu: "997990029962006639",
+			taiko: "997990028741451848",
+			fruits: "997990032466006026",
+			mania: "997990031216091217",
+		};
+
+		const request = emojis[name];
+
+		return !request ? "" : request;
 	}
 }
