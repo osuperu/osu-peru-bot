2a8fcedff95d7e90a31699c0852e15dc94f31568
FlyingCat-X <flyingcatdm@gmail.com>
2022-07-20 17:32:34 -0500

    Improve embed response

--- src/server/discord/responses/compare.ts
+++ src/server/discord/responses/compare.ts
@@ -1,4 +1,4 @@
-import { MessageOptions } from "discord.js";
+import { MessageEmbed, MessageOptions } from "discord.js";
 import { CommandResponse } from "../../models/command-response";
 import { OUser } from "../../models/osu-api/user";
 import { OBeatmap } from "../../models/osu-api/beatmap";
@@ -13,7 +13,6 @@ export class CompareResponse implements CommandResponse {
 		scores: OScores,
 		gamemode: OGamemodeName
 	): Promise<MessageOptions> {
-		let description = "";
 		let count = 1;
 
 		if (scores.scores.length === 0) {
@@ -21,41 +20,53 @@ export class CompareResponse implements CommandResponse {
 				content: `${user.username} no tiene puntuciones en el mapa.`,
 			};
 		}
+
+		const embed = new MessageEmbed()
+			.setAuthor({
+				name: `${user.username}: ${user.statistics.pp}pp (#${
+					user.statistics.global_rank || 0
+				} | ${user.country_code.toUpperCase}: #${
+					user.statistics.rank.country
+				})`,
+				url: `https://osu.ppy.sh/users/${user.id}`,
+				iconURL: user.avatar_url,
+			})
+			.setTitle(
+				`${beatmap.beatmapset.artist} - ${beatmap.beatmapset.title} [${beatmap.version}]`
+			)
+			.setURL(`https://osu.ppy.sh/b/${beatmap.id}`)
+			.setThumbnail(
+				`https://b.ppy.sh/thumb/${beatmap.beatmapset.id}l.jpg`
+			)
+			.setDescription(
+				`Descargas: [Bancho](https://osu.ppy.sh/d/${beatmap.id}) • [Beatconnect](https://beatconnect.io/b/${beatmap.id}) • [Chimu](https://api.chimu.moe/v1/download/${beatmap.id})`
+			);
+
 		for (const score of scores.scores) {
 			const mods = score.mods.length > 0 ? ` ${score.mods.join("")}` : "";
 
-			description +=
-				`**${count}.** ${mods}` +
-				"\n" +
-				`${score.rank} • ${
-					score.pp ? score.pp.toFixed(0) : undefined
-				}pp › ${(score.accuracy * 100).toFixed(2)}% › <${
-					score.max_combo
-				}x/${beatmap.max_combo}x> › \`[${Misc.getHitsFor(
-					gamemode,
-					score
-				)}]\`` +
-				"\n" +
-				`Hace ${Misc.timeFromNow(score.created_at)}` +
-				"\n";
+			embed.addField(
+				`**${count}. Mods:** ${mods}`,
+				`•  ${score.rank} ▸ **${
+					score.pp ? score.pp.toFixed(2) : 0
+				}pp** ▸ ${(score.accuracy * 100).toFixed(2)}%` +
+					"\n" +
+					`Puntaje: ${Misc.toNumberWithCommas(
+						score.score
+					)} ▸ Combo: ${beatmap.max_combo}x/${
+						beatmap.max_combo
+					}x ▸ [${Misc.getHitsFor(gamemode, score)}]` +
+					"\n" +
+					`Puntuacion hecha <t:${Misc.convertToUnixTimestamp(
+						score.created_at
+					)}:R>`
+			);
 
 			count++;
 		}
 
 		return {
-			embeds: [
-				{
-					author: {
-						icon_url: user.avatar_url,
-						name: `Top de jugadas de ${user.username} en ${beatmap.beatmapset.artist} - ${beatmap.beatmapset.title} [${beatmap.version}]| osu!${gamemode}`,
-					},
-					url: `https://osu.ppy.sh/b/${beatmap.id}`,
-					description: description,
-					thumbnail: {
-						url: `https://b.ppy.sh/thumb/${beatmap.beatmapset.id}l.jpg`,
-					},
-				},
-			],
+			embeds: [embed],
 		};
 	}
 }
--- src/server/discord/responses/recent.ts
+++ src/server/discord/responses/recent.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
-import { MessageOptions } from "discord.js";
+import { MessageEmbed, MessageOptions } from "discord.js";
 import { CommandResponse } from "../../models/command-response";
 import { OUser } from "../../models/osu-api/user";
 import { OScore } from "../../models/osu-api/score";
 import { OGamemodeName } from "../../models/osu-api/gamemode";
 import { PerformanceFactory } from "../../util/performance-factory";
+import { Misc } from "../../util/misc";
 
 export class RecentResponse implements CommandResponse {
 	async getMessage(
@@ -20,36 +21,46 @@ export class RecentResponse implements CommandResponse {
 			.calculate(beatmapFile.data, score);
 		const mods = score.mods.length > 0 ? ` +${score.mods.join("")}` : "";
 
+		const embed = new MessageEmbed()
+			.setAuthor({
+				name: `${user.username}: ${user.statistics.pp}pp (#${
+					user.statistics.global_rank || 0
+				} | ${user.country_code.toUpperCase()}: #${
+					user.statistics.rank.country
+				})`,
+				url: `https://osu.ppy.sh/users/${user.id}`,
+				iconURL: user.avatar_url,
+			})
+			.setTitle(
+				`${score.beatmapset.artist} - ${score.beatmapset.title} [${
+					score.beatmap.version
+				}] +${mods} [${computedPerformance.starRating.toFixed(2)}★]`
+			)
+			.setURL(`https://osu.ppy.sh/b/${score.beatmap.id}`)
+			.setThumbnail(`https://b.ppy.sh/thumb/${score.beatmapset.id}l.jpg`)
+			.setDescription(
+				`Descargas: [Bancho](https://osu.ppy.sh/d/${score.beatmap.id}) • [Beatconnect](https://beatconnect.io/b/${score.beatmap.id}) • [Chimu](https://api.chimu.moe/v1/download/${score.beatmap.id})` +
+					"\n\n" +
+					`•  ${score.rank} ▸ ${
+						(score.pp ? score.pp.toFixed(2) : 0) ||
+						computedPerformance.recentPP.toFixed(2)
+					}pp ▸ ${(
+						score.accuracy * 100
+					).toFixed(2)}%` +
+					"\n" +
+					`Puntaje: ${Misc.toNumberWithCommas(
+						score.score
+					)} ▸ Combo: ${score.max_combo}x/${
+						computedPerformance.maxCombo
+					}x ▸ [${Misc.getHitsFor(gamemode, score)}]` +
+					"\n\n" +
+					`Puntuacion hecha <t:${Misc.convertToUnixTimestamp(
+						score.created_at
+					)}:R>`
+			);
+
 		return {
-			embeds: [
-				{
-					author: {
-						icon_url: score.user.avatar_url,
-						name: `Score reciente de osu!${
-							score.mode != "osu" ? score.mode : ""
-						} jugado por ${score.user.username}`,
-					},
-					url: `https://osu.ppy.sh/b/${score.beatmap.id}`,
-					title: `${score.beatmapset.artist} - ${
-						score.beatmapset.title
-					} [${
-						score.beatmap.version
-					}] (${computedPerformance.starRating.toFixed(2)}★${mods})`,
-					description: `${score.rank} • ${
-						(score.pp ? score.pp.toFixed(0) : undefined) ||
-						Math.round(computedPerformance.recentPP)
-					}pp › ${(score.accuracy * 100).toFixed(2)}% › <${
-						score.max_combo
-					}x/${computedPerformance.maxCombo}x> › \`[${
-						computedPerformance.hitsInfo
-					}]\``,
-					thumbnail: {
-						url: `https://b.ppy.sh/thumb/${score.beatmapset.id}l.jpg`,
-					},
-					color: "#f45592",
-					timestamp: new Date(score.created_at),
-				},
-			],
+			embeds: [embed],
 		};
 	}
 }
