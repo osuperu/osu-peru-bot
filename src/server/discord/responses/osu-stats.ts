6c5ff132db0c34c7167b84fae558f624a027fce6
FlyingCat-X <flyingcatdm@gmail.com>
2022-07-20 17:26:01 -0500

    Rename osu-stats response ts file

--- src/server/discord/responses/osu-stats.ts
+++ src/server/discord/responses/osu-stats.ts
@@ -7,7 +7,7 @@ import { OGamemodeName } from "../../models/osu-api/gamemode";
 export class OsuStatsResponse implements CommandResponse {
 	async getMessage(user: OUser, gamemode: OGamemodeName): Promise<MessageOptions> {
 		return {
-			embeds: [
+			embeds: [	
 				{
 					author: {
 						name: user.username,
@@ -21,17 +21,14 @@ export class OsuStatsResponse implements CommandResponse {
 						`▸ **Rank:** #${user.statistics.global_rank || 0} (${user.country.name} #${user.statistics.rank.country || 0})` + "\n" +
 						`▸ **Nivel:** ${user.statistics.level.current} (${user.statistics.level.progress}%)` + "\n" +
 						`▸ **Total PP:** ${user.statistics.pp}` + "\n" +
-						`▸ **Precision:** ${user.statistics.hit_accuracy.toFixed(2)}` + "\n" +
+						`▸ **Precision:** ${user.statistics.hit_accuracy.toFixed(2)}%` + "\n" +
 						`▸ **Conteo de jugadas:** ${user.statistics.play_count} (${Misc.secondsToHours(user.statistics.play_time)} hrs)` + "\n",
 					footer: {
 						text: `Anteriores nombres de usuario: ${
 							user.previous_usernames.join(", ") ||
 							"El usuario no ha tenido otros nombres de usuario"
 						}`,
-						/*
-                        icon_url: `https://raw.githubusercontent.com/ppy/osu-wiki/master/wiki/shared/mode/${
-                            gamemode === "fruits" ? "catch" : gamemode
-                        }.png`,*/
+                        icon_url: `https://cdn.discordapp.com/emojis/${Misc.getEmojiID(gamemode)}.webp`,
 					},
 				},
 			],
